abfa2ebb592eed496932eac6e0c2536c73c79421
Daniel Schutz <daniel3322schutz@hotmail.com>
2024-10-04 20:37:55 +0100

    step 1 done

--- server/src/main/java/dadkvs/server/DadkvsServerState.java
+++ server/src/main/java/dadkvs/server/DadkvsServerState.java
@@ -1,25 +1,78 @@
 package dadkvs.server;
 
-public class DadkvsServerState {
-    boolean        i_am_leader;
-    int            debug_mode;
-    int            base_port;
-    int            my_id;
-    int            store_size;
-    KeyValueStore  store;
-    MainLoop       main_loop;
-    Thread         main_loop_worker;
+import dadkvs.DadkvsMain;
+import dadkvs.DadkvsMainServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
-    
+import java.util.ArrayList;
+import java.util.List;
+
+public class DadkvsServerState {
+    boolean i_am_leader;
+    int debug_mode;
+    int base_port;
+    int my_id;
+    int store_size;
+    KeyValueStore store;
+    MainLoop main_loop;
+    Thread main_loop_worker;
+    private int sequenceNumber;
+    private List<DadkvsMainServiceGrpc.DadkvsMainServiceBlockingStub> serverStubs;
+
     public DadkvsServerState(int kv_size, int port, int myself) {
-	base_port = port;
-	my_id = myself;
-	i_am_leader = false;
-	debug_mode = 0;
-	store_size = kv_size;
-	store = new KeyValueStore(kv_size);
-	main_loop = new MainLoop(this);
-	main_loop_worker = new Thread (main_loop);
-	main_loop_worker.start();
+        base_port = port;
+        my_id = myself;
+        i_am_leader = (myself == 0);
+        debug_mode = 0;
+        store_size = kv_size;
+        store = new KeyValueStore(kv_size);
+        main_loop = new MainLoop(this);
+        main_loop_worker = new Thread(main_loop);
+        main_loop_worker.start();
+        sequenceNumber = 0;
+        initializeServerStubs();
+    }
+
+    private void initializeServerStubs() {
+        serverStubs = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            if (i != my_id) {
+                String host = "localhost";
+                int port = base_port + i;
+                ManagedChannel channel = ManagedChannelBuilder.forAddress(host, port)
+                        .usePlaintext()
+                        .build();
+                DadkvsMainServiceGrpc.DadkvsMainServiceBlockingStub stub = DadkvsMainServiceGrpc
+                        .newBlockingStub(channel);
+                serverStubs.add(stub);
+            }
+        }
+    }
+
+    public synchronized int getNextSequenceNumber() {
+        return ++sequenceNumber;
+    }
+
+    public boolean isLeader() {
+        return i_am_leader;
+    }
+
+    public void broadcastOrderedRequest(int seqNum, DadkvsMain.CommitRequest request) {
+        DadkvsMain.CommitRequest orderedRequest = DadkvsMain.CommitRequest.newBuilder(request)
+                .setReqid(seqNum)
+                .build();
+
+        for (DadkvsMainServiceGrpc.DadkvsMainServiceBlockingStub stub : serverStubs) {
+            try {
+                stub.committx(orderedRequest);
+            } catch (Exception e) {
+                System.err.println("Failed to send ordered request to a server: " + e.getMessage());
+            }
+        }
+    }
+
+    public DadkvsMainServiceGrpc.DadkvsMainServiceBlockingStub getLeaderStub() {
+        return serverStubs.get(0);
     }
 }
